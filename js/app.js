f1bae0b41af7a87ec031c86556abbfde32b86b7f
Miles Lemon <miles@bigbluedoor.net>
2018-04-09 09:22:31 +0100

    established array of different cards to be shuffled

--- js/app.js
+++ js/app.js
@@ -36,3 +36,24 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+// array of card icons
+let cardIcons = [
+    'fa-diamond',
+    'fa-diamond',    
+    'fa-anchor',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-bolt',
+    'fa-cube',
+    'fa-cube',
+    'fa-leaf',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bicycle',
+    'fa-bomb',
+    'fa-bomb',
+    'fa-paper-plane-o',
+    'fa-paper-plane-o'
+];
